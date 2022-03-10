6017c886a9d26d4e0118afe60dcf9ba235fa39be
GstrXXX <101074574+GstrXXX@users.noreply.github.com>
2022-03-10 14:24:09 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,20 @@
+var cantidadDeVentanasEnMiCasa = 15;
+console.log(cantidadDeVentanasEnMiCasa);
+
+var soyHumano = true
+console.log(soyHumano)
+
+var miGustoDePizzaFavorito = 'alemana'
+console.log(miGustoDePizzaFavorito)
+
+var misma = false
+console.log(misma)
+
+misma = 'Hola'
+console.log(misma)
+
+misma = 0
+console.log(misma)
+
+misma = 'Minions'
+console.log(misma)
